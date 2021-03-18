a214e42f2ca2518253649c57a74d20cf58216e17
PL2Z35 <43555170+PL2Z35@users.noreply.github.com>
2021-03-17 19:45:06 -0500

    Falta interfaz

---
+++ src/helpers/auth.js
@@ -0,0 +1,12 @@
+
+const helpers = {};
+
+helpers.isAuthenticated = (req, res, next) => {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    req.flash('error', 'Not Authorized.');
+    res.redirect('/users/signin');
+};
+
+module.exports = helpers;
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,8 +4,4 @@ router.get('/', (req, res) => {
     res.render('index')
 });
 
-router.get('/about', (req, res) => {
-    res.render('about')
-});
-
 module.exports = router;
